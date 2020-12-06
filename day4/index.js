93a6fcb1bc8fb70ec69a5a2059bbd79efccfb46b
gixxerblade <sdclarkie@gmail.com>
2020-12-06 10:12:44 -0500

    day4 update

--- day4/index.js
+++ day4/index.js
@@ -1,12 +1,27 @@
 // Day 4 part 1
 const fs = require("fs");
-
+/* 
+'byr', (Birth Year)
+'iyr', (Issue Year)
+'eyr', (Expiration Year)
+'hgt', (Height)
+'hcl', (Hair Color)
+'ecl', (Eye Color)
+'pid', (Passport ID)
+'cid', (Country ID)
+*/
+const required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+let obj = {},
+  valid = 0;
 fs.readFile("testdata.txt", "utf-8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  let passports = data.split("\n\n")
+  let passports = data
+    .toString()
+    .split("\n\n")
+    .map((n) => n.replace(/\n/g, " "))
+    .map((el) => el.split(" ").replace(/(:[A-Za-z0-9])\w+)/g, ""))
+    console.log(passports);
 });
-
-let obj = {};
